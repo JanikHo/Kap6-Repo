1e640f5ab67dba277487f992ac1aa2251ea48782
Janik Hopf <janikhopf@MacBook-Air-von-Janik.local>
2023-03-14 23:18:13 +0100

    Adds more functions with vector type

--- vector.cpp
+++ vector.cpp
@@ -4,18 +4,35 @@
 using namespace std;
 
 int main(){
-    vector<int> myvector(5);
+    // vector<int> myvector(5);
 
-    myvector[0] = 10;
-    cout << "Enter Value 2" << endl;
-    cin >> myvector[1];
+    // myvector[0] = 10;
+    // cout << "Enter Value 2" << endl;
+    // cin >> myvector[1];
     
-    for (int i = 0; i < myvector.size()+1; i++)
-    {
-        cout << myvector[i] << endl;
-       // cout << myvector.at(i) << endl;
+    // for (int i = 0; i < myvector.size()+1; i++)
+    // {
+    //     cout << myvector[i] << endl;
+    //    // cout << myvector.at(i) << endl;
+    // }
+
+    // cout << "Size: " << myvector.size() << endl;
+
+    vector<double> getrunken;
+    double now;
+    do{
+        cout << "Wie viel hast du getrunken?: " << endl;
+        cin >> now;
+        if(now != -1){
+            getrunken.push_back(now);
+        }
+    }while (now != -1);
+
+    double gesamt = 0.0;
+    for(double &val : getrunken){
+        gesamt += val;
     }
 
-    cout << "Size: " << myvector.size() << endl;
+    cout << "Heute getrunken: " << gesamt << endl;
     return 0;
 }
