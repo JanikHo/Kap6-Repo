1946ae85833a184415c7fd4ef71d0bfcc22258ff
Janik Hopf <janikhopf@MacBook-Air-von-Janik.local>
2023-03-14 22:49:37 +0100

    Adds 1d array

--- main.cpp
+++ main.cpp
@@ -1,4 +1,30 @@
-int main(){
+#include <iostream>
 
+using namespace std;
+
+
+int main(){
+    int myarray[3] = {1, 2, 3};
+
+    cout << myarray << endl;
+
+    for (int i = 0; i < 3; i++)
+    {
+        cout << myarray[i] << endl;
+    }
+
+    cout << "Array 2: \n";
+     int myarray2[5] = {1, 2};
+
+    // modify array
+    myarray2[2] = 10;
+    // Klassische Ausdrucksweise
+    for (int i = 0; i < sizeof(myarray2)/sizeof(int); i++)
+    {
+        cout << myarray2[i] << endl;
+    }
+
+
+   
     return 0;
 }
