a894a0ae711f972d3a4c6c1245b08a9690075eec
Janik Hopf <janikhopf@MacBook-Air-von-Janik.local>
2023-03-14 23:14:00 +0100

    Adds some vector operations

--- vector.cpp
+++ vector.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 int main(){
+    vector<int> myvector(5);
 
+    myvector[0] = 10;
+    cout << "Enter Value 2" << endl;
+    cin >> myvector[1];
+    
+    for (int i = 0; i < myvector.size()+1; i++)
+    {
+        cout << myvector[i] << endl;
+       // cout << myvector.at(i) << endl;
+    }
+
+    cout << "Size: " << myvector.size() << endl;
     return 0;
 }
